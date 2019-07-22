4fd375532621b9d963f530bdf1ab6ccebc4d1096
Pedram <tadayoni@gmail.com>
2019-07-21 22:10:45 -0700

    Add missing space to sed command

--- kafka/tasks/install_kafka.sh
+++ kafka/tasks/install_kafka.sh
@@ -5,7 +5,7 @@ cd kafka-docker
 
 hostip=$(ifconfig eth0 | awk 'sub(/inet /,""){print $1}')
 
-sed -i 's/KAFKA_ADVERTISED_HOST_NAME:/&'"$hostip"' #/' docker-compose.yml
+sed -i 's/KAFKA_ADVERTISED_HOST_NAME:/&'" $hostip"' #/' docker-compose.yml
 
 docker rm -f kafka-docker_kafka_1
 docker rm -f kafka-docker_zookeeper_1
