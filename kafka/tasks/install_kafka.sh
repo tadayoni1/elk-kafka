0e80d4bdaee4e3a9f7dee4f2eb76e46de55fc449
Pedram <tadayoni@gmail.com>
2019-07-21 21:46:10 -0700

    Add ansible playbooks to deploy elasticsearch, logstash and kibana, Add bash script to deploy kafka

---
+++ kafka/tasks/install_kafka.sh
@@ -0,0 +1,13 @@
+cd /home/ubuntu
+git clone https://github.com/wurstmeister/kafka-docker
+
+cd kafka-docker
+
+hostip=$(ifconfig eth0 | awk 'sub(/inet /,""){print $1}')
+
+sed -i 's/KAFKA_ADVERTISED_HOST_NAME:/&'"$hostip"' #/' docker-compose.yml
+
+docker rm -f kafka-docker_kafka_1
+docker rm -f kafka-docker_zookeeper_1
+
+docker-compose up
