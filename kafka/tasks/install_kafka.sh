1cf62e27a1df8881906cab9733acbc6821197594
Pedram <tadayoni@gmail.com>
2019-07-22 00:23:14 -0700

    Expose 9092 port for kafka in install_kafka.sh

--- kafka/tasks/install_kafka.sh
+++ kafka/tasks/install_kafka.sh
@@ -9,6 +9,12 @@ git reset --hard origin/master
 
 hostip=$(ifconfig eth0 | awk 'sub(/inet /,""){print $1}')
 
+"9092"
+if grep '\- "9092"' docker-compose.yml
+then 
+    sed -i 's/"9092/&:9092/' docker-compose.yml
+fi
+
 if grep "KAFKA_ADVERTISED_HOST_NAME:"" $hostip" docker-compose.yml
 then 
     sed -i 's/KAFKA_ADVERTISED_HOST_NAME:/&'" $hostip"' #/' docker-compose.yml
