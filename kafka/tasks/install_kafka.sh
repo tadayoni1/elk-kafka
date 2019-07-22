157f1b5856591534e848ee6e77c0e3f7206a76cb
Pedram <tadayoni@gmail.com>
2019-07-21 23:02:38 -0700

    Add restart: always to install_kafka.sh

--- kafka/tasks/install_kafka.sh
+++ kafka/tasks/install_kafka.sh
@@ -8,6 +8,18 @@ cd kafka-docker
 hostip=$(ifconfig eth0 | awk 'sub(/inet /,""){print $1}')
 
 sed -i 's/KAFKA_ADVERTISED_HOST_NAME:/&'" $hostip"' #/' docker-compose.yml
+if grep "kafka:" docker-compose.yml
+then
+    echo
+else
+    sed -i '/kafka:/a\ \ \ \ restart: always' docker-compose.yml
+fi
+if grep "zookeeper:" docker-compose.yml
+then
+    echo
+else
+    sed -i '/zookeeper:/a\ \ \ \ restart: always' docker-compose.yml
+fi
 
 docker rm -f kafka-docker_kafka_1
 docker rm -f kafka-docker_zookeeper_1
