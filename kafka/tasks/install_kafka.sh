0d01c60b839775554b9ec3fc35bd02d918badb27
Pedram <tadayoni@gmail.com>
2019-07-22 01:00:00 -0700

    Replace zookeeper hostname

--- kafka/tasks/install_kafka.sh
+++ kafka/tasks/install_kafka.sh
@@ -14,16 +14,30 @@ then
     sed -i 's/"9092/&:9092/' docker-compose.yml
 fi
 
-if ! grep "KAFKA_ADVERTISED_HOST_NAME:"" $hostip" docker-compose.yml; then sed -i 's/KAFKA_ADVERTISED_HOST_NAME:/&'" $hostip"' #/' docker-compose.yml
+if ! grep "KAFKA_ADVERTISED_HOST_NAME:"" $hostip" docker-compose.yml
+then 
+    sed -i 's/KAFKA_ADVERTISED_HOST_NAME:/&'" $hostip"' #/' docker-compose.yml
+fi
+
+if ! grep "KAFKA_ZOOKEEPER_CONNECT: zookeeper:2181" docker-compose.yml
+then 
+    sed -i 's/KAFKA_ZOOKEEPER_CONNECT:/& kafka-docker_zookeeper_1:2181 #/' docker-compose.yml
+fi
 
 if grep "kafka:" docker-compose.yml
 then
-    if ! grep "restart: always"; then sed -i '/kafka:/a\ \ \ \ restart: always' docker-compose.yml
+    if ! grep "restart: always" docker-compose.yml
+    then
+        sed -i '/kafka:/a\ \ \ \ restart: always' docker-compose.yml
+    fi
 fi
 
 if grep "zookeeper:" docker-compose.yml
 then
-    if ! grep "restart: always"; then sed -i '/zookeeper:/a\ \ \ \ restart: always' docker-compose.yml
+    if ! grep "restart: always" docker-compose.yml
+    then
+        sed -i '/zookeeper:/a\ \ \ \ restart: always' docker-compose.yml
+    fi
 fi
 
 docker rm -f kafka-docker_kafka_1
