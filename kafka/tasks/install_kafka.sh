4f499c0f5d45d556cdfae5390fc0baf1006633a0
Pedram <tadayoni@gmail.com>
2019-07-22 00:50:51 -0700

    Fix if then statement in install_kafka.sh

--- kafka/tasks/install_kafka.sh
+++ kafka/tasks/install_kafka.sh
@@ -9,31 +9,21 @@ git reset --hard origin/master
 
 hostip=$(ifconfig eth0 | awk 'sub(/inet /,""){print $1}')
 
-"9092"
 if grep '\- "9092"' docker-compose.yml
 then 
     sed -i 's/"9092/&:9092/' docker-compose.yml
 fi
 
-if [ ! grep "KAFKA_ADVERTISED_HOST_NAME:"" $hostip" docker-compose.yml ]
-then 
-    sed -i 's/KAFKA_ADVERTISED_HOST_NAME:/&'" $hostip"' #/' docker-compose.yml
-fi
+if ! grep "KAFKA_ADVERTISED_HOST_NAME:"" $hostip" docker-compose.yml; then sed -i 's/KAFKA_ADVERTISED_HOST_NAME:/&'" $hostip"' #/' docker-compose.yml
 
 if grep "kafka:" docker-compose.yml
 then
-    if [ ! grep "restart: always"]
-    then
-        sed -i '/kafka:/a\ \ \ \ restart: always' docker-compose.yml
-    fi
+    if ! grep "restart: always"; then sed -i '/kafka:/a\ \ \ \ restart: always' docker-compose.yml
 fi
 
 if grep "zookeeper:" docker-compose.yml
 then
-    if [ ! grep "restart: always"]
-    then
-        sed -i '/zookeeper:/a\ \ \ \ restart: always' docker-compose.yml
-    fi
+    if ! grep "restart: always"; then sed -i '/zookeeper:/a\ \ \ \ restart: always' docker-compose.yml
 fi
 
 docker rm -f kafka-docker_kafka_1
