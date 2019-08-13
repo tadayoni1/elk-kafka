471ec3cef72e5af7de21a16d58bc90f061393814
Pedram Tadayoni <tadayoni@gmail.com>
2019-08-12 22:58:51 -0700

    feat: option to run terraform apply/plan/destroy

--- start.sh
+++ start.sh
@@ -1,5 +1,18 @@
+while getopts e:i:r:m: option
+do
+case "${option}"
+in
+m) TERRAFORM_MODE=${OPTARG};;
+esac
+done
+
+if [ -z $TERRAFORM_MODE ]; then
+  # by default it runs 'terraform apply'. With -m you can run 'terraform plan' or 'terraform destroy'
+  TERRAFORM_MODE="apply"
+fi
+
 pushd hardware
 terraform init
 
-terraform apply -auto-approve -var-file="parameters/parameters.tfvars"
+terraform ${TERRAFORM_MODE} -auto-approve -var-file="parameters/parameters.tfvars"
 popd
